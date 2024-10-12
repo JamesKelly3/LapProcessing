11f996f5484e2a7eefe443af047a54eba6e1a5d7
James Kelly <jamesakelly@hotmail.co.uk>
2024-10-12 12:10:03 +0100

    Add results companion files, and parse out pit stop times

--- processing.py
+++ processing.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import textwrap
+import json
 
 def get_replay(url):
     resp = requests.get(url)
@@ -24,6 +25,16 @@ def parse_replay(replay, filter=True):
         laps[drivers[lap['driverId']]].append((lap['lapNumber'], lap['lapTimeMS']/1000, lap['isValid'], lap['topSpeedKMH']))
 
     return filter_laps(laps) if filter else laps
+
+def parse_acc_rc_dump(file):
+    with open(file) as acc_rc_dump:
+        parsed = json.load(acc_rc_dump)
+        pit_times = defaultdict(dict)
+        for lap in parsed["laps"]:
+            if lap["pitTime"] > 10 * 1000: # file sometimes has tiny pitstops (I guess a bug in acc_rc) *1000 because it's ms
+                pit_times[lap["driverNickName"]][lap["lapNumber"]] = lap["pitTime"]
+    return pit_times
+
 
 def mean_var_from_laps(laps):
     ret = {}
@@ -78,7 +89,6 @@ def plot_max_speed(laps):
     f, ax = plt.subplots(tight_layout=True)
     f.set_figwidth(15)
     maxes = [np.mean([lap[3] for lap in l]) for l in laps.values()]
-    #maxes = [sorted(l, key=lambda x: x[3])[-1][3] for l in laps.values()]
     x = [textwrap.fill(d, width=15) for (d, _s) in laps.keys()]
     ax.bar(x, maxes, color=[DRIVER_COLOURS[s] for (_d, s) in laps.keys()])
     for i in range(len(x)):
@@ -88,6 +98,17 @@ def plot_max_speed(laps):
     ax.set_ylabel("Max Speed (KPH)")
     ax.set_ylim([min(maxes)*0.98, max(maxes)*1.02])
     plt.xticks(rotation=0, fontsize=8)
+
+def plot_pit_times(pit):
+    f, ax = plt.subplots(tight_layout=True)
+    f.set_figwidth(15)
+    i = 0
+    for driver, times in pit.items():
+        for lap_no, time_ms in times.items():
+            ax.bar(f"{driver} Lap: {lap_no}", time_ms/1000, color=DRIVER_COLOURS[driver])
+            plt.text(i, time_ms/1000, f"{time_ms/1000}", ha='center')
+            i+=1
+    ax.set_title("Pit stop times")
 
 def plot_gaps(laps):
     f, ax = plt.subplots()
@@ -120,7 +141,7 @@ DRIVER_COLOURS = {
 }
 
 REPLAYS = {
-    'bathhurst': {
+    'bathurst': {
         "2024": {
             "race": "https://www.accreplay.com/api/replays/31871",
             "qualifying": "",
@@ -200,17 +221,20 @@ REPLAYS = {
         "alien_time": 92.4,
     }
 }
+
 plt.rcParams['figure.figsize'] = [11, 8]
 track = "misano"
 year = "2024"
 session = "race"
 laps = parse_replay(get_replay(REPLAYS[track][year][session]), filter=True)
 unfiltered_laps = parse_replay(get_replay(REPLAYS[track][year][session]), filter=False)
+pit = parse_acc_rc_dump(f"ACC_companion_dumps/{track}.json")
 alien_time = REPLAYS[track]["alien_time"]
 plot_max_speed(laps)
 plot_norms(laps, alien_time)
 plot_laps(laps)
 plot_sorted_laps(laps, alien_time)
 plot_gaps(unfiltered_laps)
+plot_pit_times(pit)
 plt.show()
 
